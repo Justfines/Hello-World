4df23e9d53b2c84392d511310450952f557649a7
Justfines <Justfiness@gmail.com>
2019-01-29 15:16:12 -0800

    added main method that prints the Hello to the world

--- src/edu/pcc/cis/justinparsley/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/justinparsley/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.justinparsley.helloworld;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+
+
 }
