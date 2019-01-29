358b4648837681c14bb98562e8764a449edd9824
Justfines <Justfiness@gmail.com>
2019-01-29 15:36:07 -0800

    added author comment

--- src/edu/pcc/cis/justinparsley/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/justinparsley/helloworld/HelloWorld.java
@@ -2,6 +2,10 @@ package edu.pcc.cis.justinparsley.helloworld;
 
 import java.util.Date;
 
+/**
+ * Created by:
+ *  Justin parsley; With the username Justfines
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
